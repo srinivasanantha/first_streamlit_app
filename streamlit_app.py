ccdec4a03ae9d416a386c78e498ed1d9b1e1ad74
srinivasanantha <105879650+srinivasanantha@users.noreply.github.com>
2022-05-19 15:58:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Serving_Size')
 # Put the picklist widget for customers to pickup fruits from list
-streamlit.multiselect("Pick the Fruits from List:", list( my_fruit_list.index), ['Avocado', 'Strawberries'] )
+streamlit.multiselect("Pick the Fruits from List:", list( my_fruit_list.index),['Avocado', 'Strawberries'] )
 #Display the Fruits list on the page
 streamlit.dataframe(my_fruit_list)
                       
