ceea716330e96d1ac727b473f7cb99886cfdfe7b
srinivasanantha <105879650+srinivasanantha@users.noreply.github.com>
2022-05-19 15:30:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,3 @@
 import streamlit
 streamlit.title( "First Streamlit program with Github testing 123...")
-streamit.text( "Check this line for any errors")
+streamlit.text( "Check this line for any errors")
