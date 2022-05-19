b4822347c6f087f41c125c2776804d5ab289f9bd
srinivasanantha <105879650+srinivasanantha@users.noreply.github.com>
2022-05-19 15:43:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Put the picklist widget for customers to pickup fruits from list
+streamlit.multiselect("Pick the Fruits from List", list( my_fruit_list.index)
+#Display the Fruits list on the page
 streamlit.dataframe(my_fruit_list)
+                      
 streamlit.title( "First Streamlit program with Github testing 123...")
 streamlit.text( "Check this line for any errors")
