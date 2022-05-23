6d9ff18593c6c8c0fac35a46f994268042480d32
srinivasanantha <105879650+srinivasanantha@users.noreply.github.com>
2022-05-23 17:15:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,5 @@ streamlit.text( "Check this line for any errors")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json() )
-fruityvice_normalized = pandas.json_normalized(fruityvice_response.json() )
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json() )
 streamlit.dataframe(fruityvice_normalized)
