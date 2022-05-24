ff730bb90ac4bbdcb292d8d58253b97b74a9dc7d
srinivasanantha <105879650+srinivasanantha@users.noreply.github.com>
2022-05-24 13:37:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,10 @@
 import streamlit
 import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-# Put the picklist widget for customers to pickup fruits from list
-fruits_selected=streamlit.multiselect("Pick the Fruits from List:", list( my_fruit_list.index),['Avocado', 'Strawberries'] )
-fruits_to_show=my_fruit_list.loc[fruits_selected]
-#Display the Fruits list on the page
-#streamlit.dataframe(my_fruit_list)
-streamlit.dataframe(fruits_to_show)
-streamlit.title( "First Streamlit program with Github testing 1111...")
-streamlit.text( "Check this line for any errors")
-# replaced with text_input ...fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-streamlit.text(fruityvice_response.json() )
-streamlit.header('Fruityvice Fruit advice!')
-import requests
-#fruit_choice = streamlit.text_input( 'What fruit would you like information about?', 'Kiwi')
-#streamlit.write('The user entered', fruit_choice)
-#fruityvice_response=requests.get("https://fruitvice.com/api/fruit/" + fruit_choice) 
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json() )
-#streamlit.dataframe(fruityvice_normalized)
-import snowflake.connector
+streamlit.title("My parents new healthy diner")
+streamlit.header("Breakfast menu")
+streamlit.text("Omega 3 & Blueberry oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothy")
+streamlit.text("Hard boiled free-range egg ")
+streamlit.header("Build your own Smoothie")
+my_fruit_list=pandas.read_csv(https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)                              
