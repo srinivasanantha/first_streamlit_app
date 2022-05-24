649ee1505f551f74fdaa326833fcf37d50542fd4
srinivasanantha <105879650+srinivasanantha@users.noreply.github.com>
2022-05-23 21:15:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Put the picklist widget for customers to pickup fruits from list
@@ -10,12 +11,11 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.title( "First Streamlit program with Github testing 1111...")
 streamlit.text( "Check this line for any errors")
-import requests
 # replaced with text_input ...fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response.json() )
 streamlit.header('Fruityvice Fruit advice!')
 fruit_choice = streamlit.text_input( 'What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
-fruitvice_response=requests.get("https://fruitvice.com/api/fruit/" + fruit_choice) 
+fruityvice_response=requests.get("https://fruitvice.com/api/fruit/" + fruit_choice) 
 #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json() )
 #streamlit.dataframe(fruityvice_normalized)
